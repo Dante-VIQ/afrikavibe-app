c302781b4f4b47e7f395d9685d684eb36ea9ebe9
Dante-VIQ <damalide20@gmail.com>
2025-09-10 10:44:25 +0300

    i added an advertisement script

--- resources/views/livewire/blog-page.blade.php
+++ resources/views/livewire/blog-page.blade.php
@@ -108,6 +108,15 @@
         </a>
     </div>
 
+    <div class="grid grid-cols-1 fix-underline wow FadeInUp">
+        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1920954764751411"
+            crossorigin="anonymous"></script>
+        <ins class="adsbygoogle" style="display:block" data-ad-format="fluid" data-ad-layout-key="-5b+cl-e-8b+of"
+            data-ad-client="ca-pub-1920954764751411" data-ad-slot="5750592805"></ins>
+        <script>
+            (adsbygoogle = window.adsbygoogle || []).push({});
+        </script>
+    </div>
 
     {{-- <div class="flex">
         @can('create', $blog)
