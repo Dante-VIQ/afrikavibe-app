337a5a5dbf92d0796bc6e62a1df59f13803d70f8
Daniel Mwangi <149162668+Dante-VIQ@users.noreply.github.com>
2025-09-12 13:42:57 +0300

    Update read-more-card.blade.php

--- resources/views/components/read-more-card.blade.php
+++ resources/views/components/read-more-card.blade.php
@@ -11,7 +11,7 @@
         </button>
     </div>
     <!-- Full Content -->
-    <div class="space-y-4 p-3 sm:p-2">
+    <div class="space-y-4 p-3 sm:p-2 overflow-scroll">
         {{ $slot }}
     </div>
 </div>
