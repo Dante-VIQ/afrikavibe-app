66d854e9b6dbacaa6b4c414bf3ab1d8cc4719b7f
Daniel Mwangi <149162668+Dante-VIQ@users.noreply.github.com>
2025-09-12 21:55:48 +0300

    Update destination.blade.php

--- resources/views/destination.blade.php
+++ resources/views/destination.blade.php
@@ -1,6 +1,6 @@
 <x-guest-layout>
 <div>
-    <livewire:doctor-page lazy />
+    <livewire:doctor-page :doctor="$doctor" :key="$doctor->id" lazy />
     {{-- <x-destinations /> --}}
 </div>
 </x-guest-layout>
