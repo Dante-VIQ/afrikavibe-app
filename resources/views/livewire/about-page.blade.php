e609197aba5fb763a26e881a1e08f881abeb1585
Daniel Mwangi <149162668+Dante-VIQ@users.noreply.github.com>
2025-09-12 17:40:23 +0300

    Update about-page.blade.php

--- resources/views/livewire/about-page.blade.php
+++ resources/views/livewire/about-page.blade.php
@@ -23,49 +23,6 @@
     </div>
     <!-- About End -->
 
-    {{-- our services --}}
-
-    <div class="row g-4">
-
-        <div>
-            <livewire:service-list />
-
-        </div>
-
-
-    </div>
-
-    <!-- Feature Start -->
-    <div class="container-fluid bg-primary overflow-hidden my-5 px-lg-0">
-        <div class="container feature px-lg-0">
-            <div>
-                <livewire:feature-card />
-            </div>
-        </div>
-    </div>
-    <!-- Feature End -->
-
-
-    <!-- Team Start -->
-    <div class="container-xxl py-5">
-        <div class="container">
-            <div class="text-center mx-auto mb-5 wow fadeInUp" data-wow-delay="0.1s" style="max-width: 600px;">
-                <p class="d-inline-block border rounded-pill py-1 px-4">Destinations</p>
-                <h1>Unforgettable Destinations</h1>
-            </div>
-            <div>
-                <livewire:doctors-card />
-            </div>
-        </div>
-    </div>
-    <!-- Team End -->
-
-
-    <!-- Footer Start -->
-    <livewire:footer-card />
-    <!-- Footer End -->
-
-
     <!-- Back to Top -->
     <a href="#" class="btn btn-lg btn-primary btn-lg-square rounded-circle back-to-top"><i
             class="bi bi-arrow-up"></i></a>
