d9b0edfc2d9a596d7a03534fc77a1b0c89207905
Daniel Mwangi <149162668+Dante-VIQ@users.noreply.github.com>
2025-09-12 15:23:13 +0300

    Update doctor-show.blade.php

--- resources/views/livewire/includes/doctor-show.blade.php
+++ resources/views/livewire/includes/doctor-show.blade.php
@@ -30,6 +30,10 @@
     {{-- <p>{{ $doctor->department }}</p> --}}
     <p class="h-24 text-sm text-gray-700 mb-3 overflow-hidden text-wrap">{{ $doctor->detail }}</p>
 
+<p class="h-24 text-sm text-gray-700 mb-3 overflow-hidden text-wrap">
+    {!! nl2br(e($doctor->detail)) !!}
+</p>
+
     <div x-data="{ open: false }" class="flex justify-evenly" x-cloak>
         <button class="mt-2 px-4 py-1 bg-orange-500 rounded-full text-white hover:bg-orange-600"
             wire:click="$dispatch('openCommentModal', {
