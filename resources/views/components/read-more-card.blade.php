0b518cdbef048a0e9ea42a8f5d2af92825cd124b
Daniel Mwangi <149162668+Dante-VIQ@users.noreply.github.com>
2025-09-12 11:32:48 +0300

    Update read-more-card.blade.php

--- resources/views/components/read-more-card.blade.php
+++ resources/views/components/read-more-card.blade.php
@@ -1,15 +1,16 @@
-<div x-show="open" class="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-50" x-transition>
-    <div class="bg-white rounded-2xl shadow-lg max-w-7xl w-full p-6 overflow-y-auto max-h-[90vh] relative"
-        @click.away="open = false">
-        <!-- Close Button -->
-        <div>
-            <button class="absolute top-4 right-4 text-gray-600 space-y-4 hover:text-black p-5" @click="open = false">
-                ✕
-            </button>
-        </div>
-        <!-- Full Content -->
-        <div class="space-y-4 ">
-            {{ $slot }}
-        </div>
+<div 
+    class="bg-white rounded-2xl shadow-lg max-w-7xl w-full p-6 overflow-y-auto max-h-[90vh] relative
+        sm:max-w-md sm:p-4 sm:rounded-xl
+        xs:max-w-xs xs:p-2 xs:rounded-lg"
+    @click.away="open = false">
+    <!-- Close Button -->
+    <div>
+        <button class="absolute top-2 right-2 text-gray-600 hover:text-black p-3 sm:p-2 xs:p-1" @click="open = false">
+            ✕
+        </button>
+    </div>
+    <!-- Full Content -->
+    <div class="space-y-4">
+        {{ $slot }}
     </div>
 </div>
