4889b979b90ebc110ba2ad20cd1c7b0f156d1fcf
Daniel Mwangi <149162668+Dante-VIQ@users.noreply.github.com>
2025-09-12 11:30:07 +0300

    Update guest.blade.php

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -78,9 +78,13 @@
 
 
         <!-- Footer Start -->
-        <div>
-            <livewire:footer-card />
-        </div>
+        <footer class="footer">
+                    <div class="d-sm-flex justify-content-center justify-content-sm-between">
+                        <span class="text-muted text-center text-sm-left d-block d-sm-inline-block">Vumbi Ventures</span>
+                        <span class="float-none float-sm-end d-block mt-1 mt-sm-0 text-center">Copyright © 2025. All
+                            rights reserved.</span>
+                    </div>
+                </footer>
         <!-- Footer End -->
 
     </div>
