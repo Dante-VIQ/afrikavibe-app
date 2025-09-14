59a96407649dafa08f564f9ed9807fd623a6ffbb
Daniel Mwangi <149162668+Dante-VIQ@users.noreply.github.com>
2025-09-14 17:37:19 +0300

    Update header-media-feed.blade.php

--- resources/views/livewire/header-media-feed.blade.php
+++ resources/views/livewire/header-media-feed.blade.php
@@ -1,10 +1,7 @@
 <div class="w-full bg-white rounded-2xl shadow-sm overflow-hidden">
     {{-- Post form --}}
-    @auth
-        @if (auth()->user() && auth()->user()->isMaster())
             @livewire('create-header-media-post')
-        @endif
-    @endauth
+     
 
     {{-- Alpine.js Media Slider --}}
     @if ($this->headerMedia->isNotEmpty())
