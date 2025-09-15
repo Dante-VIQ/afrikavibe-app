62083a5c62b02c0fed82d96b3467346d11bdf95b
Daniel Mwangi <149162668+Dante-VIQ@users.noreply.github.com>
2025-09-15 09:45:45 +0300

    Update doctor-page.blade.php

--- resources/views/livewire/doctor-page.blade.php
+++ resources/views/livewire/doctor-page.blade.php
@@ -28,7 +28,12 @@
             @unless (count($doctors) == 0)
                 @foreach ($doctors as $doctor)
                     <x-destination-card wire:loading.remove>
-                        @include('livewire.includes.doctor-show')
+                    @php
+    $formatted = collect(preg_split("/\n\s*\n/", e($doctor->detail)))
+        ->map(fn($p) => "<p>{$p}</p>")
+        ->implode('');
+@endphp
+    @include('livewire.includes.doctor-show')
                     </x-destination-card>
                 @endforeach
             @else
