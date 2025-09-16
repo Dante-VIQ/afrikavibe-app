0b9a76fa19d74734b49cb6f93ce4a829075be05b
Daniel Mwangi <149162668+Dante-VIQ@users.noreply.github.com>
2025-09-16 11:33:34 +0300

    Update about-card.blade.php

--- resources/views/livewire/about-card.blade.php
+++ resources/views/livewire/about-card.blade.php
@@ -1,41 +1,7 @@
 <div>
     {{-- <div class="row g-5"> --}}
     @foreach ($abouts as $about)
-        <section class="bg-white">
-            <div class="gap-16 items-center py-8 px-4 mx-auto max-w-screen-xl lg:grid lg:grid-cols-2 lg:py-16 lg:px-6">
-                <div class="font-light text-gray-500 sm:text-lg">
-                    @if ($editingAboutID == $about->id)
-                    <div class="mb-2 text-black font-semibold">
-                        <label for="NewTitle" class="inline-block text-lg mb-2">title</label>
-                        <input wire:model.live="NewTitle" type="text"
-                            class="border border-gray-200 rounded p-2 w-full" name="NewTitle"
-                            value="{{ old('NewTitle') }}" />
-
-                        @error('NewTitle')
-                            <p class="text-red-500 text-xs mt-1">{{ $message }}</p>
-                        @enderror
-                    </div>
-                    @endif
-                    <h2 class="mb-4 text-4xl tracking-tight font-extrabold text-gray-900">
-                        {{ $about->title }}</h2>
-                    <p class="mb-4">{{ $about->detail }}</p>
-                </div>
-
-                <div class="grid grid-cols-2 gap-4 mt-8">
-
-                    <img class="w-full h-72 max-h-full max-w-full rounded-lg"
-                        src="{{ asset('storage/' . $about->image) }}" alt="{{ $about->title }}">
-
-
-                    {{-- <img class="w-full h-72 max-h-full max-w-full rounded-lg" src="{{ $about->image }}" alt="{{ $about->name}}" > --}}
-
-                    <img class="mt-6 w-full h-72 max-h-full lg:mt-10 rounded-lg"
-                        src="{{ asset('storage/' . $about->photo) }}" alt="{{ $about->title }}" />
-
-
-                </div>
-            </div>
-            </section>
+        
     @endforeach
 
     {{-- </div> --}}
@@ -66,6 +32,6 @@
                 </div>
 
             </div>
-        @endcand
+        @endcan
     </div>
 </div>
