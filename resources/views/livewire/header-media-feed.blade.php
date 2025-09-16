a387adf7bce90e6f59c8d338ad212663f7f79227
Daniel Mwangi <149162668+Dante-VIQ@users.noreply.github.com>
2025-09-16 10:51:20 +0300

    Update header-media-feed.blade.php

--- resources/views/livewire/header-media-feed.blade.php
+++ resources/views/livewire/header-media-feed.blade.php
@@ -54,7 +54,7 @@
                 <div>
                     {{-- Previous Button --}}
                     <button @click="previous()"
-                        class="absolute left-4 top-1/2 transform -translate-y-1/2 z-30 bg-black/50 text-white p-3 rounded-full hover:bg-black/70 transition-colors">
+                        class="absolute left-4 top-1/2 transform -translate-y-1/2 z-30 bg-black/30 text-white p-3 rounded-full hover:bg-black/50 transition-colors">
                         <svg class="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 19l-7-7 7-7">
                             </path>
@@ -63,7 +63,7 @@
 
                     {{-- Next Button --}}
                     <button @click="next()"
-                        class="absolute right-4 top-1/2 transform -translate-y-1/2 z-30 bg-black/50 text-white p-3 rounded-full hover:bg-black/70 transition-colors">
+                        class="absolute right-4 top-1/2 transform -translate-y-1/2 z-30 bg-black/30 text-white p-3 rounded-full hover:bg-black/50 transition-colors">
                         <svg class="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 5l7 7-7 7">
                             </path>
