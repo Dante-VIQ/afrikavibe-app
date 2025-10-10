4e5b363f966c8911ee4bbe58730a7d8a88fa78b7
Daniel Mwangi <149162668+Dante-VIQ@users.noreply.github.com>
2025-10-10 19:49:32 +0300

    Update ads.blade.php

--- resources/views/layouts/partials/ads.blade.php
+++ resources/views/layouts/partials/ads.blade.php
@@ -1,7 +1,11 @@
- <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1920954764751411"
-            crossorigin="anonymous"></script>
-        <ins class="adsbygoogle" style="display:block" data-ad-format="fluid" data-ad-layout-key="-5b+cl-e-8b+of"
-            data-ad-client="ca-pub-1920954764751411" data-ad-slot="5750592805"></ins>
-        <script>
-            (adsbygoogle = window.adsbygoogle || []).push({});
-        </script>
+<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1920954764751411"
+     crossorigin="anonymous"></script>
+<ins class="adsbygoogle"
+     style="display:block"
+     data-ad-format="fluid"
+     data-ad-layout-key="-5c+cl-e-8b+of"
+     data-ad-client="ca-pub-1920954764751411"
+     data-ad-slot="9020630650"></ins>
+<script>
+     (adsbygoogle = window.adsbygoogle || []).push({});
+</script>
